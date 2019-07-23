4885f1bf2b3f3f51ba7a79e0f2d8b8d09ee78aaf
Alex Peter <apyells@gmail.com>
2019-07-23 15:04:16 -0500

    works up to 'testing GET /articles' section

--- src/app.js
+++ src/app.js
@@ -3,15 +3,25 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
-const { NODE_ENV } = require("./config")
+const { NODE_ENV } = require("./config");
+const ArticlesService = require("./articles-service");
 
 const app = express();
 
-const morganOption = (NODE_ENV === 'production') ? "tiny" : "common";
+const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(morgan(morganOption));
-app.use(helmet());
 app.use(cors());
+app.use(helmet());
+
+app.get("/articles", (req, res, next) => {
+  const knexInstance = req.app.get('db')
+  ArticlesService.getAllArticles(knexInstance)
+    .then(articles => {
+      res.json(articles);
+    })
+    .catch(next);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
